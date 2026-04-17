f3e9865115a95b4210fc6bc5eab19a4db7a5e529
litterthanlit <nikolay96g@gmail.com>
2026-04-17 17:49:49 +0300

    feat(tokens): add Inter / Fraunces / JetBrains Mono and editorial color tokens

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/fraunces/400.css'
+import '@fontsource/fraunces/500.css'
+import '@fontsource/jetbrains-mono/400.css'
 import App from './App.tsx'
 import './index.css'
 
